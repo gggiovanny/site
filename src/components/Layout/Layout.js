bb6b0a4598c3de07958e877f61602dde0af8d93f
gggiovanny <giovanny.gonzalez.19@gmail.com>
2024-03-31 19:13:13 -0600

    adds logo image and improves layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,71 +1,33 @@
-import styled from '@emotion/styled';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import { author } from '../../utils/config';
-
-const Header = styled.header`
-  padding-top: 3rem;
-  padding-bottom: 1rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  font-family: 'Raleway', sans-serif;
-  font-optical-sizing: auto;
-  font-weight: 500;
-  font-style: normal;
-
-  /* flex stuff */
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-
-  a {
-    /* styles that makes links to not look like links */
-    text-decoration: none;
-    color: inherit;
-  }
-`;
-
-const LogoContainer = styled.div`
-  font-size: 1.5rem;
-`;
-
-const Nav = styled.nav`
-  ul {
-    list-style-type: none;
-    margin: 0;
-    overflow: hidden;
-  }
-`;
-
-const Page = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Main = styled.main`
-  padding: 1rem;
-  max-width: 800px;
-`;
+import { Header, HeaderContainer, LogoContainer, Main, Nav, Page } from './styles';
 
 export const Layout = ({ pageTitle, children }) => {
   return (
     <Page>
-      <Header>
-        <LogoContainer>
-          <Link to="/">{author.name}</Link>
-        </LogoContainer>
-        <Nav>
-          <ul>
-            <li>
-              <a href={`mailto:${author.email}`}>@contact</a>
-            </li>
-          </ul>
-        </Nav>
-      </Header>
+      <HeaderContainer>
+        <Header>
+          <LogoContainer>
+            <Link to="/">
+              <StaticImage
+                height={40}
+                src="../../images/header-logo.png"
+                alt="giovanny.baltazar logo"
+              />
+            </Link>
+          </LogoContainer>
+          <Nav>
+            <ul>
+              <li>
+                <a href={`mailto:${author.email}`}>@contact</a>
+              </li>
+            </ul>
+          </Nav>
+        </Header>
+      </HeaderContainer>
       <Main>
         {pageTitle && <h1>{pageTitle}</h1>}
         {children}
---
+++ src/components/Layout/styles.js
@@ -0,0 +1,64 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const centeredContentCss = css`
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const HeaderContainer = styled.header`
+  padding-top: 3rem;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+export const Header = styled.div`
+  ${centeredContentCss}
+
+  font-family: 'Raleway', sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  font-size: 1.2rem;
+  /* flex stuff */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    /* styles that makes links to not look like links */
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export const LogoContainer = styled.div``;
+
+export const Nav = styled.nav`
+  li {
+    color: #aaa;
+    transition: color 0.2s ease;
+  }
+
+  li:hover {
+    color: #111;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    overflow: hidden;
+  }
+`;
+
+export const Page = styled.div``;
+
+export const Main = styled.main`
+  padding: 1rem;
+  ${centeredContentCss}
+`;
