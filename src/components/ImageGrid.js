28c3d268fa09ac80b878835b350acc21282ebeb9
Giovanny González Baltazar <giovanny@mifiel.com>
2024-04-01 23:28:53 -0600

    fixes responsivness on mobile

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -2,6 +2,10 @@ import styled from '@emotion/styled';
 
 export const ImageGrid = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
   grid-gap: 1rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
 `;
