03cca70b11338481f49d2e48e1d8cf04de4e2eaa
Giovanny González Baltazar <giovanny@mifiel.com>
2024-04-05 20:17:45 -0600

    fixes async script reducing asincroness #15

--- scripts/watermark.js
+++ scripts/watermark.js
@@ -6,29 +6,22 @@ const path = require('path');
 const photosPath = path.join(__dirname, '../src/images/photos');
 const watermarkPath = path.join(__dirname, '../src/images/watermark.png');
 
-console.log(`Adding watermark located at ${watermarkPath}...`);
-
-eachFile(photosPath, galleryFolder => {
-  const galleryPath = path.join(photosPath, galleryFolder);
-  eachFile(galleryPath, photoName => {
-    processImage(galleryPath, photoName);
+const addWatermark = (originalFilePath, newFilePath) =>
+  new Promise((resolve, reject) => {
+    sharp(originalFilePath)
+      .composite([{ input: watermarkPath, gravity: 'southeast' }])
+      .toFile(newFilePath, (err, info) => {
+        if (err) {
+          console.error(`Error processing image ${originalFilePath}:`, err);
+          reject();
+        } else {
+          console.log('Image processed successfully:', { originalFilePath, ...info });
+          resolve();
+        }
+      });
   });
-});
 
-function eachFile(dir, loopCallback) {
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      console.error('Error reading directory:', err);
-      return;
-    }
-
-    files.forEach(file => {
-      loopCallback(file);
-    });
-  });
-}
-
-function processImage(galleryPath, photoName) {
+async function processImage(galleryPath, photoName) {
   if (photoName === 'cover.jpg') return;
 
   const originalFilePath = path.join(galleryPath, photoName);
@@ -36,13 +29,29 @@ function processImage(galleryPath, photoName) {
   const [basename, extension] = photoName.split('.');
   const newFilePath = path.join(galleryPath, `${basename}-watermarked.${extension}`);
 
-  sharp(originalFilePath)
-    .composite([{ input: watermarkPath, gravity: 'southeast' }])
-    .toFile(newFilePath, (err, info) => {
-      if (err) {
-        console.error(`Error processing image ${originalFilePath}:`, err);
-      } else {
-        console.log('Image processed successfully:', { originalFilePath, ...info });
-      }
-    });
+  await addWatermark(originalFilePath, newFilePath);
+  fs.unlinkSync(newFilePath);
+  fs.renameSync(originalFilePath, newFilePath);
 }
+
+const getPhotoPaths = () =>
+  fs
+    .readdirSync(photosPath)
+    .map(galleryFolder => {
+      const galleryPath = path.join(photosPath, galleryFolder);
+      return fs.readdirSync(galleryPath).map(photoName => ({
+        galleryPath,
+        photoName,
+      }));
+    })
+    .flat();
+
+async function main() {
+  const processImagePromises = getPhotoPaths().map(({ galleryPath, photoName }) =>
+    processImage(galleryPath, photoName)
+  );
+
+  await Promise.allSettled(processImagePromises);
+}
+
+main();
