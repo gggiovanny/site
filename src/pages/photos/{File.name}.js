2528987a063b227c621ae1ef75b6e3ba8a82d944
Giovanny González Baltazar <giovanny@mifiel.com>
2024-11-25 23:00:43 -0600

    adds technical description on photo page

--- src/pages/photos/{File.name}.js
+++ src/pages/photos/{File.name}.js
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
+import styled from '@emotion/styled';
 import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import React from 'react';
@@ -6,13 +7,26 @@ import React from 'react';
 import { Layout, Seo } from '../../components';
 import NotFoundPage from '../404';
 
+const Description = styled.div`
+  margin-top: 1rem;
+  text-align: center;
+  color: #888;
+
+  // animate color change on hover
+  transition: color 0.2s ease;
+
+  &:hover {
+    color: #111;
+  }
+`;
+
 function ImageCategoryPage({ data: { file } }) {
   if (!file?.childImageSharp?.gatsbyImageData) return <NotFoundPage />;
 
   return (
     <Layout>
       <GatsbyImage image={file.childImageSharp.gatsbyImageData} alt={file.name} />
-      {file.name}
+      <Description>{file.fields.technicalDescription}</Description>
     </Layout>
   );
 }
@@ -24,6 +38,7 @@ export const query = graphql`
       name
       fields {
         category
+        technicalDescription
       }
       childImageSharp {
         gatsbyImageData(width: 4096, placeholder: BLURRED)
