7c928fe5e442f4dabf9adcaefac3840e1c2be778
Giovanny González Baltazar <giovanny@mifiel.com>
2025-05-28 00:27:44 -0600

    Revert "fixes some of the imagegrid layout"
    This reverts commit b8a340d12316772fc18a168f61c77bc5ee18a87b.

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const ImageGrid = ({ children }) => {
-  return <section className="inline-grid grid-cols-3 gap-[16px]">{children}</section>;
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">{children}</section>
+  );
 };
