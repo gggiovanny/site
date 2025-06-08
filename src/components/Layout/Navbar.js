d09013d8c3fcb2dc85eb3c076a73631663cc2cba
Giovanny González Baltazar <giovanny@mifiel.com>
2025-06-08 12:59:08 -0600

    Adds new routes to the navbar layout

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -5,24 +5,30 @@ import { RiArrowLeftSLine } from 'react-icons/ri';
 export const Navbar = ({ parentUrl }) => {
   return (
     <header className="py-6 font-raleway">
-      <div className="flex flex-row justify-between items-center">
+      <nav className="flex flex-row items-center justify-between" aria-label="Main navigation">
         <div>
-          <Link to="/" className="">
-            about
+          <Link className="text-xl font-bold" to="/">
+            Giovanny Gonzalez
           </Link>
         </div>
-        <nav>
-          <ul className="list-none m-0 overflow-hidden leading-none">
-            {parentUrl && (
-              <li>
-                <Link to={parentUrl} className="text-gray-500 hover:text-gray-900 no-underline">
-                  <RiArrowLeftSLine size={40} color="gray" />
-                </Link>
-              </li>
-            )}
-          </ul>
-        </nav>
-      </div>
+        <ul className="flex flex-row gap-3 font-semibold list-none m-0 p-0">
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/collections">Galleries</Link>
+          </li>
+          <li className={parentUrl ? 'opacity-100' : 'opacity-0 pointer-events-none'}>
+            <Link
+              to={parentUrl}
+              className="text-gray-500 hover:text-gray-900 no-underline"
+              aria-label="Back"
+            >
+              <RiArrowLeftSLine size={24} color="gray" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
