33152c9bd6829c341d4759ef0cf720acf4722c11
Giovanny González Baltazar <giovanny@mifiel.com>
2025-06-08 12:33:05 -0600

    removes tailwind settings tha I think are unnecesary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        raleway: ['Raleway', 'sans-serif'],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
